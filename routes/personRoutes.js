3e598bf2095f2cee5dae662a610283cc4c9f09f0
Luis Henrique <luishpmenezes@hotmail.com>
2025-06-09 22:32:25 -0300

    go

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,13 +1,21 @@
+// api/persons.js
 const express = require('express');
-const router = express.Router();
 const db = require('../models');
+const app = express();
 
-// GET /api/persons
-router.get('/persons', async (req, res) => {
-    const persons = await db.Person.findAll({
-        include: [db.Education, db.Experience, db.Project, db.Skill]
-    });
-    res.json(persons);
+app.use(express.json());
+
+app.get('/api/persons', async (req, res) => {
+    try {
+        const persons = await db.Person.findAll({
+            include: [db.Education, db.Experience, db.Project, db.Skill],
+        });
+        res.json(persons);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
-module.exports = router;
+
+module.exports = app;
+module.exports.handler = require('serverless-http')(app);
