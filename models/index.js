61092aaea4318df6f2c26e3746ae5c0ad14889d8
Luis Henrique <luishpmenezes@hotmail.com>
2025-06-14 00:42:48 -0300

    add

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
             rejectUnauthorized: false, // Necessário para Render e outros hosts cloud
         },
     },
+    dialectModule: require("pg"),
 });
 
 const db = {};
